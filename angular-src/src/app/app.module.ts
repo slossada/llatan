c11b5e1dab99e1510eaf21376309597074747122
slossada <santilossada@gmail.com>
2018-08-28 22:45:33 -0400

    Componente de Registro, Validacion y Mensajes

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes} from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -34,7 +35,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  nombre: String;
+  seg_nombre: String;
+  apellido: String;
+  cedula: String;
+  email: String;
+  username: String;
+  password: String;
+
+  constructor(
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      nombre: this.nombre,
+      seg_nombre: this.seg_nombre,
+      apellido: this.apellido,
+      cedula: this.cedula,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Require all fields
+    if (user.nombre == undefined || user.apellido == undefined || user.cedula == undefined || user.email == undefined || user.username == undefined || user.password == undefined) {
+      this.flashMessage.show('Por favor llene todas las casillas.', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+
+    // Validate Email
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regex.test(user.email.toLowerCase())) {
+      this.flashMessage.show('Por favor ingrese una dirección de correo válida.', { cssClass: 'alert-danger', timeout: 5000 });
+      return false;
+    }
   }
 
 }
